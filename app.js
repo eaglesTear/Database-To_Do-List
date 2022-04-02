c1ee0c545cfe2cd2281d4a6be6063cd63ef7e39c
Ryan Hadley <eagle_tears@yahoo.com>
2022-04-02 15:57:03 +0100

    Modified app.js

--- app.js
+++ app.js
@@ -170,10 +170,10 @@ app.get("/about", function (req, res) {
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+//if (port == null || port == "") {
+//    port = 3000;
+//}
 
-app.listen(port, function () {
-    console.log("Server started successfully.");
+app.listen(port || 3000, function() {
+  console.log("Server has started Successfully on dynamic port ");
 });
